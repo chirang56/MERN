033e1d9d9cc11458b13d0ada6a3b085c0fc901fa
chirang56 <tamgaubijiranchi114@gmail.com>
2025-09-02 23:54:53 +0545

    Fix response messages and improve consistency in API endpoints

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,20 @@ import express from 'express';
 const app = express();
 app.get('/api/notes', (req, res) => {
     // send all notes
-    res.status(200).send("You have got your notes here");
+    res.status(200).send("You have got your notes here!");
 });
 
 app.post("/api/notes", (req, res) => {
     // add or create a new note
-    res.status(201).send("note added successfully");
+    res.status(201).json({ message: "Note added successfully!"});
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({ messge: "Note updated successfully!"});
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note deleted successfully!"});
 });
 
 app.listen(5001, () => {

4a009fc8f9d432e438e54d93e6409e0b37a4600a
chirang56 <tamgaubijiranchi114@gmail.com>
2025-09-04 11:31:16 +0545

    Implement notes API with CRUD operations and set up routing

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,15 @@
+export const getAllNotes = (req, res) => {
+    res.status(200).send("You just fetched the notes!");
+}
+
+export function createNote(req, res) {
+    res.status(201).json({ message: "Note added successfully!" });
+}
+
+export function updateNote(req, res) {
+    res.status(200).json({ message: "Note updated successfully!" });
+}
+
+export function deleteNote(req, res) {
+    res.status(200).json({ message: "Note deleted successfully!" });
+}
